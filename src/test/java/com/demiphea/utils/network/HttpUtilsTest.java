0d96f5853db9b73d194e04c203aee28c4dd5015c
demiphea <demiphea@163.com>
2024-05-07 13:59:26 +0800

    feat: 完成基本HTTP服务端请求工具类。

---
+++ src/test/java/com/demiphea/utils/network/HttpUtilsTest.java
@@ -0,0 +1,45 @@
+package com.demiphea.utils.network;
+
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * HttpUtilsTest
+ *
+ * @author demiphea
+ * @since 17.0.9
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class HttpUtilsTest {
+    private static final String TEST_URL = "https://www.baidu.com";
+
+    @Test
+    void get() {
+        assertDoesNotThrow(() -> {
+            ClassicHttpResponse response = HttpUtils.get(TEST_URL);
+            assertEquals(HttpStatus.SC_OK, response.getCode());
+            response.close();
+
+            String strResponse = HttpUtils.simpleGet(TEST_URL);
+            assertNotNull(strResponse);
+            assertFalse(strResponse.isBlank());
+        });
+    }
+
+    @Test
+    void post() {
+        assertDoesNotThrow(() -> {
+            ClassicHttpResponse response = HttpUtils.post(TEST_URL);
+            assertEquals(HttpStatus.SC_OK, response.getCode());
+            response.close();
+
+            String strResponse = HttpUtils.simplePost(TEST_URL);
+            assertNotNull(strResponse);
+            assertFalse(strResponse.isBlank());
+        });
+    }
+}
