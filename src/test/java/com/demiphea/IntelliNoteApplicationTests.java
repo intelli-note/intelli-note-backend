2a85902cb9cf158850b2b184f2f9b8c2d5b920ef
demiphea <demiphea@163.com>
2024-05-23 21:09:26 +0800

    fix: 修正测试。

--- src/test/java/com/demiphea/IntelliNoteApplicationTests.java
+++ src/test/java/com/demiphea/IntelliNoteApplicationTests.java
@@ -2,8 +2,10 @@ package com.demiphea;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest
+@ActiveProfiles("test")
 //@ExtendWith(MockitoExtension.class)
 class IntelliNoteApplicationTests {
 
