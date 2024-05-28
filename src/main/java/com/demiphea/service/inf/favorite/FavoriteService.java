292963d69ec95447cd5a69cb58254f7a696d65a3
demiphea <demiphea@163.com>
2024-05-28 20:44:22 +0800

    feat: 完成接口-分页获取收藏夹列表。

--- src/main/java/com/demiphea/service/inf/favorite/FavoriteService.java
+++ src/main/java/com/demiphea/service/inf/favorite/FavoriteService.java
@@ -1,8 +1,10 @@
 package com.demiphea.service.inf.favorite;
 
+import com.demiphea.model.api.PageResult;
 import com.demiphea.model.po.favorite.FavoritePo;
 import com.demiphea.model.vo.favorite.FavoriteVo;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * FavoriteService
@@ -39,4 +41,17 @@ public interface FavoriteService {
      * @author demiphea
      */
     void deleteFavorite(@NotNull Long id, @NotNull Long favoriteId);
+
+
+    /**
+     * 分页获取用户收藏夹
+     *
+     * @param id       当前用户ID
+     * @param userId   查询用户ID
+     * @param pageNum  页码
+     * @param pageSize 每页数量
+     * @return {@link PageResult} 分页结果
+     * @author demiphea
+     */
+    PageResult listFavorites(@Nullable Long id, @Nullable Long userId, @NotNull Integer pageNum, @NotNull Integer pageSize);
 }
