49103b277ff46db237abc54a65f39b367e75cf14
demiphea <demiphea@163.com>
2024-06-05 19:19:41 +0800

    fix: 修复测试。

--- src/test/java/com/demiphea/utils/network/HttpUtilsTest.java
+++ src/test/java/com/demiphea/utils/network/HttpUtilsTest.java
@@ -1,10 +1,16 @@
 package com.demiphea.utils.network;
 
+import com.demiphea.utils.reflect.CommonReflectionUtils;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.HttpStatus;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,10 +20,23 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author demiphea
  * @since 17.0.9
  */
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//@ActiveProfiles("test")
 public class HttpUtilsTest {
     private static final String TEST_URL = "https://www.baidu.com";
+
+    @BeforeAll
+    static void beforeAll() {
+        CommonReflectionUtils.setStaticFieldValue(HttpUtils.class,
+                "client", HttpClients.createDefault()
+        );
+    }
+
+    @AfterAll
+    static void afterAll() throws IOException {
+        CloseableHttpClient client = (CloseableHttpClient) CommonReflectionUtils.getStaticFieldValue(HttpUtils.class, "client");
+        client.close();
+    }
 
     @Test
     void get() {
