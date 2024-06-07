d776df656571f982a6018f288f3928af28ba091b
demiphea <demiphea@163.com>
2024-06-07 14:51:51 +0800

    fix: 新增笔记时采用默认封面。

--- src/main/java/com/demiphea/common/Constant.java
+++ src/main/java/com/demiphea/common/Constant.java
@@ -1,5 +1,7 @@
 package com.demiphea.common;
 
+import cn.hutool.core.util.RandomUtil;
+
 import java.util.List;
 
 /**
@@ -18,4 +20,11 @@ public class Constant {
     public static final List<String> COLLECTION_COVER_DIR = List.of("intelli-note", "collection", "cover");
 
     public static final List<String> TMP_DIR = List.of("intelli-note", "tmp");
+
+
+    private static final String DEFAULT_NOTE_COVER_URL = "https://cdn.demiphea.com/intelli-note/default/note-cover-<NUMBER>.png";
+
+    public static String defaultNoteCover() {
+        return DEFAULT_NOTE_COVER_URL.replace("<NUMBER>", String.valueOf(RandomUtil.randomInt(0, 6)));
+    }
 }
